43495667cb6a24ec1d475fcd04c5f8cd3601c4e4
Marcus Shaqir <chkir44@gmail.com>
2024-05-11 18:11:20 +0000

    added function for computer choice and what possibilties avaible to choose from , added const index = Math.floor(Math.random() * choices.length); to generate random choices

--- script.js
+++ script.js
@@ -1,12 +1,24 @@
 let wins = 0;
 let losses = 0;
-let triesLeft = 6; 
+let triesLeft = 5;
 
 function play(userChoice) {
     if (triesLeft === 0) {
-        document.getElementById('result').innerText = 'Game over! Reload to try again';
+        document.getElementById('result').innerText = 'Game over! Reload to play again';
         return;
     }
-    const computerChoice = getComputerChoice(); 
+
+    const computerChoice = getComputerChoice();
     const result = determineWinner(userChoice, computerChoice);
-    
+
+    document.getElementById('result').innerText = `You chose ${userChoice}. Computer chose ${computerChoice}. ${result}`;
+    document.getElementById('win').innerText = wins;
+    document.getElementById('loss').innerText = losses;
+    document.getElementById('tries-left').innerText = --triesLeft;
+}
+
+function getComputerChoice() {
+    const choices = ['rock', 'paper', 'scissors'];
+    const index = Math.floor(Math.random() * choices.length);
+    return choices[index];
+}
