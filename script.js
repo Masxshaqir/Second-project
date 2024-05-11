523d043905663d10a25ad7f0313b7345baaeada0
Marcus Shaqir <chkir44@gmail.com>
2024-05-11 16:12:32 +0000

    Function to handle a single play session and Check if no tries left and notify the user of game over,Exit if no tries are left

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 let wins = 0;
 let losses = 0;
 let triesLeft = 6; 
+
+function play(userChoice) {
+    if (triesLeft === 0) {
+        document.getElementById('result').innerText = 'Game over! Reload to play again';
+        return;
+    }
